9384f35b3d08fda934a08191a62ea530b19eca18
GastonMosteiro <gastonmosteiro@hotmail.com>
2023-08-14 20:54:13 -0300

    update

--- src/main/java/logic/controllers/ControllerAltaUsuario.java
+++ src/main/java/logic/controllers/ControllerAltaUsuario.java
@@ -6,11 +6,36 @@ import logic.Profesor;
 
 public class ControllerAltaUsuario implements IControllerAltaUsuario {
 
+    /*
+     * Actores: Administrador del sistema
+     * 
+     * Description: El caso de uso comienza cuando el administrador desea dar de
+     * alta a un nuevo usuario en el sistema. Para ello indica si es un/a socio/a o
+     * un profesor/a y sus datos básicos: nickname (único), nombre, apellido,
+     * correo electrónico (único) y fecha de nacimiento. Si el usuario es un
+     * profesor/a se ingresan además otros datos básicos: la institución a la
+     * cual pertenece, una descripción general, una breve biografía (opcional) y
+     * un link a su sitio web (opcional). Si el nickname o el correo electrónico se
+     * encuentran en uso por algún otro usuario, el sistema avisa al administrador,
+     * pudiendo éste corregir la información o cancelar el alta. Finalmente, el
+     * sistema da de alta al usuario.
+     */
+
     @Override
     public String addProfesor(String nickname, String nombre, String apellido, String email, DtFecha fechaNac,
             String descripcion, String biografia, String sitioWeb) {
         try {
-            return "asda";
+
+            if (validateUserData(nickname, email)){
+                return "Error:  ";
+            }
+
+            //Code
+            return "";
+
+            
+
+
         } catch (Exception errorException) {
             System.out.println("AddProfesor catch: " + errorException);
             return "AddProfesor catch: " + errorException;
@@ -21,6 +46,11 @@ public class ControllerAltaUsuario implements IControllerAltaUsuario {
     @Override
     public String addSocio(String nickname, String nombre, String apellido, String email, DtFecha fechaNac) {
         return "";
+    }
+
+    private boolean validateUserData(String nickname, String Email) {
+        // TO DO
+        return true;
     }
 
 }
