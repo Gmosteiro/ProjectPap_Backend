a7c464b178879f8c4dec2a804a45e892e5498c83
santiago saret <santiagosaret@santiagos-MacBook-Air.local>
2023-08-14 10:03:08 -0300

    fabrica y manejadores

---
+++ src/main/java/logic/manejadores/ManejadorUsuario.java
@@ -0,0 +1,20 @@
+package logic.manejadores;
+
+import java.util.ArrayList;
+import java.util.List;
+import logic.Usuario;
+
+public class ManejadorUsuario {
+
+	//private List<DtUsuario> usuarios;
+
+	public ManejadorUsuario() {
+	//	usuarios = new ArrayList<>();
+	}
+
+//	public void agregarUsuario(DtUsuario nuevoUsuario) {
+//		usuarios.add(nuevoUsuario);
+//	}
+
+	
+}
