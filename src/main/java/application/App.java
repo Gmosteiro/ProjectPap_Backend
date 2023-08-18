d53e662dd6ab3971bb7976e2a5d7a7177911222d
AlexRibm <117485971+AlexRibm@users.noreply.github.com>
2023-08-18 20:12:04 -0300

    Clase I

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,8 +1,5 @@
 package application;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import logic.Fabrica;
-import logic.Clase.controllers.*;
+
 public class App {
 
 public static void main(String[] args) {
@@ -17,24 +14,24 @@ public static void main(String[] args) {
         e.printStackTrace();
         }
 
-        String esi = "ESI";
+        /*String esi = "ESI";
         LocalDate fecha = LocalDate.of(2023, 8, 17);
         LocalTime hora = LocalTime.of(14, 30);
         String campus = "Campus";
         LocalDate fechareg = LocalDate.of(2023, 8, 17);
- 		 try {
-                Fabrica factory = new Fabrica();
-
-                IControllerAltaClase controllerAltaClase = factory.getControladorAltaClase();
-
-
-                controllerAltaClase.addClase(esi, fecha, hora, campus, fechareg);
-
-            } catch (Exception e) {
-                System.out.println("Catch main: " + e.getMessage());
-                e.printStackTrace();
-            }
-
+        try {
+        Fabrica factory = new Fabrica();
+        
+        IControllerAltaClase controllerAltaClase = factory.getControladorAltaClase();
+        
+        
+        controllerAltaClase.addClase(esi, fecha, hora, campus, fechareg);
+        
+        } catch (Exception e) {
+        System.out.println("Catch main: " + e.getMessage());
+        e.printStackTrace();
         }
-
+        
+        }*/
+}
 }
