89e95b262f6a235473e44880bfea825683d13389
xSanPrax <92343442+xSanPrax@users.noreply.github.com>
2023-08-28 08:49:13 -0300

    eso del nombre

---
+++ src/main/java/logic/ActividadDeportiva/controllers/IControllerModificaActividad.java
@@ -0,0 +1,5 @@
+package logic.ActividadDeportiva.controllers;
+
+public interface IControllerModificarActividad {
+    void modificarActividad(String nombre, String nuevaDescripcion, int nuevaDuracion, float nuevoCosto);
+}
