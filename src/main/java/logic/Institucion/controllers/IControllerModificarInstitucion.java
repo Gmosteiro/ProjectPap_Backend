3c8233677fa80859c150cf4a7c6f256c67a1cc09
xSanPrax <92343442+xSanPrax@users.noreply.github.com>
2023-08-28 09:04:12 -0300

    ModificarInstitucion

---
+++ src/main/java/logic/Institucion/controllers/IControllerModificarInstitucion.java
@@ -0,0 +1,5 @@
+package logic.Institucion.controllers;
+
+public interface IControllerModificarInstitucion {
+    void modificarInstitucion(String nombre, String nuevaDescripcion, String nuevaURL);
+}
