e40db09bcd6e493fca642223d0f3f31fa172c0fb
xSanPrax <92343442+xSanPrax@users.noreply.github.com>
2023-08-28 21:18:50 -0300

    quedo chelo

--- src/main/java/logic/Clase/controllers/ControllerConsultaClases.java
+++ src/main/java/logic/Clase/controllers/ControllerConsultaClases.java
@@ -2,30 +2,75 @@ package logic.Clase.controllers;
 
 import logic.Clase.Clase;
 import logic.Clase.ManejadorClases;
+import logic.Institucion.InstitucionDeportiva;
+import logic.ActividadDeportiva.ActividadDeportiva;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 public class ControllerConsultaClases implements IControllerConsultaClases {
-    private final ManejadorClases manejadorClases;
+    private EntityManagerFactory emf;
 
     public ControllerConsultaClases() {
-        manejadorClases = new ManejadorClases();
+        emf = Persistence.createEntityManagerFactory("project_pap");
     }
 
     public List<String> obtenerActividadesDeportivasPorInstitucion(String nombreInstitucion) {
-        // Lógica para obtener las actividades deportivas por institución
-        // Puedes implementar esta lógica aquí o en el manejador si lo prefieres
-        // Retorna una lista de nombres de actividades
-        return null;
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+
+            Query query = em.createQuery(
+                    "SELECT i FROM InstitucionDeportiva i WHERE i.nombre = :nombreInstitucion",
+                    InstitucionDeportiva.class);
+            query.setParameter("nombreInstitucion", nombreInstitucion);
+
+            InstitucionDeportiva institucion = (InstitucionDeportiva) query.getSingleResult();
+
+            List<String> nombresActividades = new ArrayList<>();
+            for (ActividadDeportiva actividad : institucion.getActividades()) {
+                nombresActividades.add(actividad.getNombre());
+            }
+
+            em.getTransaction().commit();
+
+            return nombresActividades;
+        } finally {
+            em.close();
+        }
     }
 
     public List<Clase> obtenerClasesPorActividad(String nombreActividad) {
-        // Lógica para obtener las clases por actividad
-        return manejadorClases.getClasesByProfe(nombreActividad);
+        ManejadorClases manejadorClases = new ManejadorClases();
+        return manejadorClases.getClasesByActividad(nombreActividad);
     }
 
     public Clase obtenerDetallesClase(String nombreClase) {
-        // Lógica para obtener los detalles de una clase
-        return null;
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+    
+            Query query = em.createQuery(
+                    "SELECT c FROM Clase c WHERE c.nombre = :nombreClase", Clase.class);
+            query.setParameter("nombreClase", nombreClase);
+    
+            Clase clase = (Clase) query.getSingleResult();
+    
+            em.getTransaction().commit();
+    
+            return clase;
+        } finally {
+            em.close();
+        }
+    }
+    
+
+    public void closeEntityManagerFactory() {
+        emf.close();
     }
 }
